16e7631e4a0b1c43f9171cc701af4a5e9cc72f64
gustavo9690 <gpalomino9690@gmail.com>
2026-02-18 03:27:20 -0500

    Ajustes en Config.php

--- backend/app/config/Config.php
+++ backend/app/config/Config.php
@@ -2,6 +2,33 @@
 
 class Config
 {
-    public static $defaultController = 'Welcome';
-    public static $defaultMethod = 'index';
+    // Entorno
+    public static string $env = 'development';
+
+    // URL base
+    public static string $baseUrl = 'http://localhost/erp/';
+
+    // Controlador y método por defecto
+    public static string $defaultController = 'Welcome';
+    public static string $defaultMethod = 'index';
+
+    // Zona horaria
+    public static string $timezone = 'America/Lima';
+
+    // Mostrar errores
+    public static bool $showErrors = true;
+
+    public static function init(): void
+    {
+        date_default_timezone_set(self::$timezone);
+
+        if (self::$showErrors) {
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL);
+        } else {
+            ini_set('display_errors', 0);
+            error_reporting(0);
+        }
+    }
 }
+
